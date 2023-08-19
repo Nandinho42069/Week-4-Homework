ff24541cbd08c972702592963594a67f6d66afa7
linuskelsey <alfred.linus.kelsey@gmail.com>
2023-08-19 08:54:57 +0100

    Configure Sepolia testnet

--- _alchemy-folder/frontend/app/layout.tsx
+++ _alchemy-folder/frontend/app/layout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { WagmiConfig, createConfig } from "wagmi";
+import { WagmiConfig, createConfig, sepolia } from "wagmi";
 import { ConnectKitProvider, getDefaultConfig } from "connectkit";
 import Navbar from "@/components/instructionsComponent/navigation/navbar";
 import Footer from "@/components/instructionsComponent/navigation/footer";
@@ -17,6 +17,7 @@ const config = createConfig(
     appDescription: "Your App Description",
     appUrl: "https://family.co", // your app's url
     appIcon: "https://family.co/logo.png", // your app's logo,no bigger than 1024x1024px (max. 1MB)
+    chains: [sepolia],
   })
 );
 
