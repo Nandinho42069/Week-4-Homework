43cb99be6009ecdf1bb05876c32d17e918977507
Nandinho42069 <129112008+Nandinho42069@users.noreply.github.com>
2023-08-20 17:37:58 +0530

    Update app.service.ts
    added token contract calling features in the constructor

--- _nest-backend/src/app.service.ts
+++ _nest-backend/src/app.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import {ethers} from "ethers";
+
+const TOKEN_ADDRESS = "";
 
 @Injectable()
 export class AppService {
+  contract;
+  provider;
+  wallet;
+
+  constructor(){
+    this.provider = new ethers.JsonRpcProvider(process.env.RPC_ENDPOINT_URL ?? "");
+    this.wallet = new ethers.Wallet(process.env.PRIVATE_KEY ?? "", this.provider);
+    this.contract = new ethers.Contract(TOKEN_ADDRESS, TokenJSON.abi, this.wallet);
+  }
+
   getHello(): string {
     return 'Hello World!';
   }
